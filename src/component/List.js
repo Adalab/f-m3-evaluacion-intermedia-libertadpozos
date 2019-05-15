3a3f05084a8b85c1b92603a498a21540265f5e54
libertadpozos <libertad.pozos@gmail.com>
2019-05-15 12:48:51 +0200

    Create function favorite, missing currentTarget

--- src/component/List.js
+++ src/component/List.js
@@ -2,14 +2,38 @@ import React from "react";
 import Card from './Card'
 
 class List extends React.Component{
+constructor(){
+  super();
+  this.state={
+      selected: "item__list"
+  };
+  this.handleSelectedItem = this.handleSelectedItem.bind(this);
+}
+
+handleSelectedItem(){
+  this.setState((prevState)=>{
+    let nextStyling;
+    if (prevState.selected==="item__list"){
+      nextStyling="selected-item";
+    }
+    else if (prevState.selected==="selected-item"){
+      nextStyling="item__list";
+    }
+ return{
+   selected:nextStyling
+ };
+  })
+}
+
 render(){
     const {pokemon}=this.props;
     console.log(pokemon)
+    const {selected}=this.state;
     return(
     <ul className="wrapper">
       {pokemon.map(item=>{
         return(
-        <li className="item__list">
+        <li className={selected} onClick={this.handleSelectedItem}>
           <Card item={item}/> 
         </li>)
       })}
