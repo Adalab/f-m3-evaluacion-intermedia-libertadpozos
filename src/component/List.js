9c8414c2ff21a3d2a07a352e307a1ae65cb79142
libertadpozos <libertad.pozos@gmail.com>
2019-05-17 04:44:49 +0200

    Change favorite to component card

--- src/component/List.js
+++ src/component/List.js
@@ -2,38 +2,15 @@ import React from "react";
 import Card from './Card'
 
 class List extends React.Component{
-constructor(){
-  super();
-  this.state={
-      selected: "item__list"
-  };
-  this.handleSelectedItem = this.handleSelectedItem.bind(this);
-}
-
-handleSelectedItem(){
-  this.setState((prevState)=>{
-    let nextStyling;
-    if (prevState.selected==="item__list"){
-      nextStyling="selected-item";
-    }
-    else if (prevState.selected==="selected-item"){
-      nextStyling="item__list";
-    }
- return{
-   selected:nextStyling
- };
-  })
-}
-
 render(){
     const {pokemon}=this.props;
     console.log(pokemon)
-    const {selected}=this.state;
+    
     return(
     <ul className="wrapper">
       {pokemon.map(item=>{
         return(
-        <li className={selected} onClick={this.handleSelectedItem}>
+        <li >
           <Card item={item}/> 
         </li>)
       })}
@@ -41,7 +18,6 @@ render(){
   
     );
 }
-
 }
 
 export default List;
